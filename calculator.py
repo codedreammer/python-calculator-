81573ba5b84202679c46057a23d15af29520458c
codedreammer <nkanal38@gmail.com>
2024-07-06 08:43:00 +0530

    learn input

--- calculator.py
+++ calculator.py
@@ -1,2 +1,8 @@
 a = input("Enter your name:")
 print("My name is", a)
+
+x = input("enter first number:")
+y = input("enter second number:")
+print(x + y)
+#to get the the value of addition 
+print(int(x) + int(y))
---
+++ input.py
@@ -0,0 +1,38 @@
+a = input("enter your name:")
+print("print my name is",a)
+
+x = input("enter first number:")
+y = input("enter second number:")
+
+print(x+y)
+
+print(int(x)+int(y))
+
+#subtraction
+a = input("enter your name:")
+print("print my name is",a)
+
+x = input("enter first number:")
+y = input("enter second number:")
+print(int(x)-int(y))
+
+#multiply
+a = input("enter your name:")
+print("print my name is",a)
+
+x = input("enter first number:")
+y = input("enter second number:")
+print(int(x)*int(y))
+
+#division
+a = input("enter your name:")
+print("print my name is",a)
+
+x = input("enter first number:")
+y = input("enter second number:")
+print(int(x)/int(y))
+
+
+
+
+
