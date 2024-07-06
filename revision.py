f27135687c3440123d49b86693c532b55ee0caa9
codedreammer <nkanal38@gmail.com>
2024-07-06 21:44:53 +0530

    learn more about string

---
+++ revision.py
@@ -0,0 +1,28 @@
+# negative indexing 
+
+b = "hello world!"
+print(b[-7:-3])
+
+a = "i am akshay mehta"
+print(a[-7:-3])
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+#F-Strings
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
