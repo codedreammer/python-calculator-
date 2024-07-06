5c28e841ffd3d54f900b2eee97d15b495e565b8c
codedreammer <nkanal38@gmail.com>
2024-07-06 09:32:15 +0530

    learn string

---
+++ string.py
@@ -0,0 +1,14 @@
+name = "akshay"
+friend = "shorya"
+anotherfrind = "lucky"
+apple = '''he said, "he want to eat apple'''
+
+print("hello,"+ name)
+#print(apple)
+print(name[0])
+print(name[1])
+
+print("lets use a for loop/n")
+for charactor in apple:
+    print(charactor)
+    
