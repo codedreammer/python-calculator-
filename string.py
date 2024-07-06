5a917f9237b7b217fe8a64545afa81d265d40e8b
codedreammer <nkanal38@gmail.com>
2024-07-06 17:40:47 +0530

    learning string method

--- string.py
+++ string.py
@@ -1,24 +1,26 @@
-name = "akshay"
-friend = "shorya"
-anotherfrind = "lucky"
-apple = '''he said, "he want to eat apple'''
+a = "!!herry!! !!!!! herry"
 
-print("hello,"+ name)
-#print(apple)
-print(name[0])
-print(name[1])
+str1 = "welcome to the console!!!"
 
-print("lets use a for loop/n")
-for charactor in apple:
-    print(charactor)
-    
-name = "akshay mehat"
-len1 = len(name)
-print(len1)
+print(len(str1))
+print(len(str1.center(50)))
+print(a.count("herry"))
 
-#Quick Quiz
-nm = "Harry"
-print(nm[-4:-2])
+str1 = "welcome to the console !!!"
+print(str1.endswith("to",4,10))
 
+str1 = "He's nmae is Dan.He is an honest man."
+print(str1.find("is"))
 
+str1 = "WelcomeToTheConsole"
+print(str1.isalnum())
+str1 ="Welcome"
+print(str1.isalpha())
 
+str1 = "WelcomeToTheConsole"
+print(str1.endswith("to",4,10))
+
+str1 = "WelcomeToTheConsole"
+print(str1.isalpha())
+str1 ="hello world"
+print(str1.islower())
