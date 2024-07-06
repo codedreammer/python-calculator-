7ac00ff15d7cb75070aeff0a17a01b2968a7b335
codedreammer <nkanal38@gmail.com>
2024-07-06 07:59:21 +0530

    add the file

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
-a = 50
-b = 3
+string = "15"
+number = 7
+string_number = int(string) #throug the error if the string is not a valid integer
+sum = number + string_number
+print("the sum of both the number is: ", sum)
 
-print("the value of",a, "+",3, "is", a+b)
-print("the value of",a, "-",3, "is", a-b)
-print("the value of",a, "*",3, "is", a*b)
-print("the value of",a, "/",3, "is", a/b)
-print("the value of",a, "%",3, "is", a%b)
+
