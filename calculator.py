5831fe28ccc363f0c7c216da70b1aa4508d6e2dd
codedreammer <nkanal38@gmail.com>
2024-07-06 08:18:12 +0530

    learn input

--- calculator.py
+++ calculator.py
@@ -1,7 +1,2 @@
-string = "15"
-number = 7
-string_number = int(string) #throug the error if the string is not a valid integer
-sum = number + string_number
-print("the sum of both the number is: ", sum)
-
-
+a = input("Enter your name:")
+print("My name is", a)
