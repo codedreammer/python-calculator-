b500bd221b539f49ff1fe28cf210c759fa0998ba
codedreammer <nkanal38@gmail.com>
2024-07-07 21:46:54 +0530

    learnig if,elif&else

--- if&else.py
+++ if&else.py
@@ -8,3 +8,32 @@ if(a>18):
 else:
     print("you cannot drive")
 
+
+applePrice = 250
+buget =  45
+if ( applePrice <= buget):
+    print("Alexa add 1 kg Apples to the cart")
+
+elif (buget - applePrice > 50):
+   print("its okay you can buy")
+       
+
+else:
+    print("Alexa,do not add Apples to the cart.")
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+  a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
