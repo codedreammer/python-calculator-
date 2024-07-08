72c9e0ab9c93f0897c69bce34067efda23e57457
codedreammer <nkanal38@gmail.com>
2024-07-08 16:13:12 +0530

    add good morning file

---
+++ good morning.py
@@ -0,0 +1,11 @@
+import time
+Name = input("Enter your name :")
+recenttime = time.strftime('%H:%M:%S')
+Recenttime = int(time.strftime('%H'))
+c= Name.capitalize()
+if(4<=Recenttime<12):
+    print('Good Morning',c,'its',recenttime)
+elif(12>=Recenttime<17):
+    print('GOOD EVENING',c,'its',recenttime)
+else:
+    print('GOOD NIGHT',c,'its',recenttime)
